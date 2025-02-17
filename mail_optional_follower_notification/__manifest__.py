41bd35925935320fb5dc8f8777817b6784afc686
Graeme Gellatly <graeme@moahub.nz>
2025-02-17 21:49:16 +1300

    [MIG][18.0] mail_optional_follower_notification

--- mail_optional_follower_notification/__manifest__.py
+++ mail_optional_follower_notification/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/mail",
     "category": "Social Network",
-    "version": "17.0.1.1.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["mail"],
     "data": [
